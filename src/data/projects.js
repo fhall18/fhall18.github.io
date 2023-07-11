a797fed0dd9efec4b5c5fd1d1252370614101212
Freddie Hall <28370009+fhall18@users.noreply.github.com@users.noreply.github.com>
2023-07-11 16:05:51 -0300

    project updates

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,10 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Heat Pump Master Equations',
+    title: 'Master Equations',
     subtitle: 'Modeling Complex Systems',
-    image: '/images/projects/nearestdollar.jpg',
+    link: 'https://github.com/fhall18/cchpMasterEquation/blob/main/thermalStates.ipynb',
+    image: '/images/projects/hp_me.jpg',
     date: '2023-06-01',
     desc:
       'Developed a master equations focused on heat pump state transfers. '
@@ -12,32 +13,21 @@ const data = [
       + 'the charity of your choice.',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
+    title: 'Commentator Bias',
+    subtitle: 'Top grade in masters level class',
     link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
+    image: '/images/projects/cycling.jpg',
+    date: '2022-12-01',
     desc:
       'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
       + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
       + 'and nutrient deficiencies at an affordable price for the developing world.',
   },
   {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
-    desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
-  },
-  {
     title: 'Cat Detector',
     subtitle: 'A convolutional neural network to classify cats! (and dogs)',
     image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
+    date: '2023-07-15',
     desc:
       'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
       + 'Over 60,000 cats were classified before server bills made the project too expensive '
