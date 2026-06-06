77f9b8ddee3052760927dca9ff5ae8e9fce60a7a
fhall18 <frederick.hall@energyhub.net>
2026-06-06 17:32:15 -0400

    Fix Jest config for react-ga4 ESM

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,12 @@ const config = {
     '^.+\\.(css|less|scss)$': 'babel-jest',
     '^.+\\.md$': 'markdown-to-jsx',
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!react-ga4)',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|mjs)$': 'babel-jest',
+  },
 };
 
 module.exports = config;
